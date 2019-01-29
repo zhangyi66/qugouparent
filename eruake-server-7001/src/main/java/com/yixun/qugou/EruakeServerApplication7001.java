701548fd8ad7ebb9b15ef65af7aa58ebfabc5d5e
zhangyi66 <263265012@qq.com>
2019-01-29 20:10:47 +0800

    试一试

--- eruake-server-7001/src/main/java/com/yixun/qugou/EruakeServerApplication7001.java
+++ eruake-server-7001/src/main/java/com/yixun/qugou/EruakeServerApplication7001.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
 @SpringBootApplication
 @EnableEurekaServer
 public class EruakeServerApplication7001 {
+    //哈哈哈哈
     public static void main(String[] args) {
         SpringApplication.run(EruakeServerApplication7001.class,args);
     }
